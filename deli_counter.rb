405a36c0d266ac16013a70bdfe8918993295eff9
Malcome Fein <mfein90@gmail.com>
2018-07-19 14:34:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+
+
+def line(katz_deli)
+  if katz_deli.length == 0
+     puts "The line is currently empty."
+  end
+end
+
+"The line is currently: 1. Grace 2. Kent 3. Matz"
+"The line is currently: 1. Kent 2. Matz"
