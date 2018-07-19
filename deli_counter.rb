6bebd04379229b953cc99c5c6a2d0c5fef63d139
Malcome Fein <mfein90@gmail.com>
2018-07-19 15:01:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,5 +18,7 @@ end
 counter = 0
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, "+ name + "." "You are number" + katz_deli.length +  "in line."
+  
+  # puts "Welcome, "+ name + "." "You are number" + katz_deli.length +  "in line."
+   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
